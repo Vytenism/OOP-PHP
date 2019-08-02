d0935b59d7e335ae5acad7e49856d21f94b4b6fc
vytenism <aparance23@gmail.com>
2019-08-02 13:18:47 +0300

    Final class Cookie

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -4,17 +4,26 @@
 namespace Core;
 
 class Cookie extends Abstracts\Cookie {
-
+    /**
+     * Constructor will set a name
+     * @param string $name
+     */
     public function __construct(string $name) {
         $this->name = $name;
     }
-
+    /**
+     * 
+     * @return void
+     */
     public function delete(): void {
         unset($_COOKIE[$this->name]);
         setcookie($this->name, -1);
         return true;
     }
-
+    /**
+     * 
+     * @return bool
+     */
     public function exists(): bool {
         if (isset($_COOKIE[$this->name])) {
             return true;
@@ -22,10 +31,13 @@ class Cookie extends Abstracts\Cookie {
 
         return false;
     }
-
+    /**
+     * 
+     * @return array
+     */
     public function read(): array {
         if ($this->exists()) {
-            $decoded = json_decode($_COOKIE[$this->name]);
+            $decoded = json_decode($_COOKIE[$this->name], true);
             if ($decoded) {
                 return $decoded;
             }
@@ -33,10 +45,14 @@ class Cookie extends Abstracts\Cookie {
         }
         return [];
     }
-
+    /**
+     * 
+     * @param type $data
+     * @param type $expires_in
+     */
     public function save($data, $expires_in = 3600): void {
         $encoded = json_encode($data);
-        setcookie($this->name, $encoded, $expires_in);
+        setcookie($this->name, $encoded, time() + $expires_in);
     }
 
 }
