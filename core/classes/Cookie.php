8d26d29f2b9fd2c55bd9ab62ff901a271668a7eb
vytenism <aparance23@gmail.com>
2019-08-02 12:22:19 +0300

    oop5

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -17,12 +17,19 @@ class Cookie extends Abstracts\Cookie {
         if (isset($_COOKIE[$this->name])) {
             return true;
         }
-        
+
         return false;
     }
 
     public function read(): array {
-        
+        $decoded = json_decode($_COOKIE[$this->name]);
+        if ($decoded === NULL) {
+            trigger_error('Not succeeded to decode!', E_USER_WARNING);
+            $decoded = [];
+            return $decoded;
+        } else {
+            return $decoded;
+        }
     }
 
     public function save($data, $expires_in = 3600): void {
