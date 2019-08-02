f003e5fa5cc22f18722d0a570778bbd5cf14f2db
vytenism <aparance23@gmail.com>
2019-08-02 11:38:32 +0300

    oop5

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -4,9 +4,9 @@
 namespace Core;
 
 class Cookie extends Abstracts\Cookie {
-    
+
     public function __construct(string $name) {
-        
+        $this->name = $name;
     }
 
     public function delete(): void {
@@ -14,7 +14,13 @@ class Cookie extends Abstracts\Cookie {
     }
 
     public function exists(): bool {
-        
+        if (file_exists($this->name)) {
+            $encoded_string = file_get_contents($this->file_name);
+
+            if ($encoded_string !== false) {
+                $this->data = json_decode($encoded_string, true);
+            }
+        }
     }
 
     public function read(): array {
